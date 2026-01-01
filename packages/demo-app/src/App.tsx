08773df8ad248dc3bf71d0e986fd2ecd3587448f
Nathan <nathan_farcasin@waters.com>
2026-01-01 13:20:06 -0500

    fix exports, remove unused code

--- packages/demo-app/src/App.tsx
+++ packages/demo-app/src/App.tsx
@@ -1,24 +1,20 @@
-import { Routes, Route } from "react-router";
-import { 
-  Navigation,
-} from "./ExampleComponents";
-import { MainDemo } from "./pages/MainDemo";
-import "./App.css";
-import { LineChartShowcase } from "./pages/LineChartShowcase";
-import InteractiveChartDemoNew from "./pages/InteractiveChartDemoNew";
+import { Routes, Route } from 'react-router'
+import { Navigation } from './ExampleComponents'
+import { MainDemo } from './pages/MainDemo'
+import './App.css'
+import InteractiveChartDemoNew from './pages/InteractiveChartDemoNew'
 
 function App() {
   return (
     <div className="font-sans bg-gray-50 min-h-screen">
       <Navigation />
-      
+
       <Routes>
         <Route path="/" element={<MainDemo />} />
-        <Route path="/linechart-showcase" element={<LineChartShowcase />} />
         <Route path="/interactive" element={<InteractiveChartDemoNew />} />
       </Routes>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
